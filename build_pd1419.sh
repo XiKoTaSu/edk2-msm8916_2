332e819da60026f9c1f56091cc3452a5d9efde59
XiKoTaSu_XKTS <3329989998@qq.com>
2026-06-26 16:22:44 +0800

    Modify build_pd1419.sh to package BootShim binary
    Update build process to include BootShim binary in the final UEFI file.

--- build_pd1419.sh
+++ build_pd1419.sh
@@ -18,13 +18,13 @@ export WORKSPACE="$PWD/workspace"
 wine asl.exe MSM8916Pkg/AcpiTables/Dsdt/Dsdt.asl
 mv DSDT.AML MSM8916Pkg/AcpiTables/DSDT.AML
 
+cd BootShim
+make REQUIRES_KERNEL_HEADER=0 FD_BASE=0x80080000 FD_SIZE=0x00200000
+cd -
 # ---- EDK2 derle (-n 4: dusuk RAM icin paralel thread siniri) ----
 GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 4 -a AARCH64 -t GCC5 -p MSM8916Pkg/Devices/pd1419.dsc
 
 # ---- Paketle: fastboot boot imaji (kerneladdr, MSM8916Pkg.fdf BaseAddress 0x80080000 ile ayni) ----
-cd BootShim
-make REQUIRES_KERNEL_HEADER=0 FD_BASE=0x80080000 FD_SIZE=0x00200000
-cd -
 FD=workspace/Build/MSM8916Pkg/DEBUG_GCC5/FV/MSM8916PKG_UEFI.fd
 DTB="${DTB:-MSM8916Pkg/pd1419.dtb}"           # repo-ici pd1419 DTB (override: DTB=/yol ./build.sh)
 INITRD="${INITRD:-workspace/dummy-initrd.gz}" # UEFI ramdisk'i kullanmaz; minimal uretilir
@@ -36,7 +36,7 @@ OUT=pd1419_uefi.img
 [ -f "$INITRD" ] || { mkdir -p workspace; printf '' | gzip -c > "$INITRD"; }
 
 echo "[paket] FD gzip + pd1419 DTB ekleniyor..."
-cat BootShim/BootShim.bin >> uefi_kernel_pd1419.gz
+cat BootShim/BootShim.bin >> MSM8916PKG_UEFI.fd
 gzip -c "$FD" > uefi_kernel_pd1419.gz
 cat "$DTB" >> uefi_kernel_pd1419.gz
 
